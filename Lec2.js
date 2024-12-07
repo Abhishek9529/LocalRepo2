814b72a28d0bd7d6ec5df06d698a520a7374dcf9
Abhishek9529 <abhishekchavhan61047@gmail.com>
2024-12-07 21:50:37 +0530

    Modified Lec2.js File

--- Lec2.js
+++ Lec2.js
@@ -47,12 +47,42 @@ multiline comment
 
 
 // logical operator : &&, ||, !
-let a = 10;
-let b = 20;
-let c = "10";
-console.log(`value of a : ${a}`);
-console.log(`value of b : ${b}`);
-console.log(a < b && a == c);
-console.log(a > b || a == c);
+// let a = 10;
+// let b = 20;
+// let c = "10";
+// console.log(`value of a : ${a}`);
+// console.log(`value of b : ${b}`);
+// console.log(a < b && a == c);
+// console.log(a > b || a == c);
 
 
+
+// Conditional Statements
+// if statements
+// let age = 13 ;
+// if (age < 18){
+//     console.log("you not eligible for voting");
+// }
+
+
+// if -else
+// let num = 5;
+// if (num % 2 == 0){
+//     console.log(`${num} is Even Number`);
+// }
+// else {
+//     console.log(`${num} is Odd Number`);
+// }
+
+
+//  if else if else
+// let num = 2;
+// if (num < 0){
+//     console.log(`${num} is Negitive Number`);
+// }
+// else if (num == 0){
+//     console.log(`${num} is Zero`);
+// }
+// else {
+//     console.log(`${num} is Positive`);
+// }
