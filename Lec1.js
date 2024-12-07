b516d2532103a072ab4f3632e5eb09cbb6a8f15d
Abhishek9529 <abhishekchavhan61047@gmail.com>
2024-12-07 18:40:12 +0530

    Add JsCodes File

---
+++ Lec1.js
@@ -0,0 +1,39 @@
+// const product = {
+//     name : "Parker Jotter Standard CT Ball Pen",
+//     color : "Black",
+//     rating : 7002,
+//     dealOfTheDay : true,
+//     rs : 270, 
+//     disscount : "5%"
+// }
+
+// console.log(product["name"]);
+// console.log(product["color"]);
+// console.log(product["rating"]);
+// console.log(product["dealOfTheDay"]);
+// console.log(product["rs"]);
+// console.log(product["disscount"]);
+
+
+// console.log(product);
+
+
+// iterating object
+// for (let key in product){
+//     console.log(`${key} : ${product[key]}`);
+// }
+
+// const profile = {
+//     name : "shradhakhapra",
+//     follow : true,
+//     message : true,
+//     posts : 195,
+//     followers : 569,
+//     following : 4,
+//     bio : "Enterpreneur",
+//     about : "Apna College | Ex-Microsoft"
+// }
+
+// for (key in  profile){
+//     console.log(`${key} : ${profile[key]}`);
+// }
---
+++ Lec2.js
@@ -0,0 +1,58 @@
+// coments in js 
+// this is single line comment 
+/*
+this 
+is
+multiline comment
+*/
+
+// operator in js 
+// = - * \ 
+//  use of operator 
+// let a = 10;
+// let b = 20;
+// console.log(`value of a : ${a}`);
+// console.log(`value of a : ${b}`);
+// console.log(`addition of both values : ${a + b}`);
+// console.log(`subtraction of both values : ${a - b}`);
+// console.log(`multiplication of both values : ${a * b}`);
+// console.log(`division of both values : ${a / b}`);
+
+
+//Assignment operator  : =, +=, -=, *=, %=, **=
+// let a = 10;
+// let b = 20;
+// console.log(`value of a : ${a}`);
+// console.log(`value of a : ${b}`);
+// console.log(b = a);
+
+// console.log(a += b);
+// console.log(a *= b);
+// console.log(a %= b);
+// console.log(a **= b);
+
+// let a = 10;
+// let b = 20;
+// let c = "10";
+// console.log(`value of a : ${a}`);
+// console.log(`value of b : ${b}`);
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+// console.log(a != b);
+// console.log(a === c);
+// console.log(a !== c);
+
+
+
+// logical operator : &&, ||, !
+let a = 10;
+let b = 20;
+let c = "10";
+console.log(`value of a : ${a}`);
+console.log(`value of b : ${b}`);
+console.log(a < b && a == c);
+console.log(a > b || a == c);
+
+
